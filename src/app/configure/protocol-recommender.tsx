c60776d45dabaa9a574a5598482382361bcad549
Tyrone Fairchild <sublyime@gmail.com>
2025-09-16 20:51:43 +0000

    Try fixing this error: `Runtime Error: Error: (0 , {imported module [pro

--- src/app/configure/protocol-recommender.tsx
+++ src/app/configure/protocol-recommender.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useActionState, useFormStatus } from 'react-dom';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { getProtocolSuggestion } from '@/app/actions';
 
 import { Button } from '@/components/ui/button';
